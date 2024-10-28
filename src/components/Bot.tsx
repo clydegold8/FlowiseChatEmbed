87348a65fe22ad89a3734666bc1cf9d18be04838
Clyde Ian Acaso <clyde.acaso@cloudemployee.co.uk>
2024-10-28 18:33:28 +0800

    Added adjustments

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1390,7 +1390,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
 
         {props.showTitle ? (
           <div
-            class="flex flex-row items-center w-full h-[50px] absolute top-0 left-0 z-10"
+            class="flex flex-row items-center w-full h-[60px] absolute top-0 left-0 z-10 rounded-xl"
             style={{
               background: props.bubbleBackgroundColor,
               color: props.bubbleTextColor,
