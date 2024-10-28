975f2cf1115d020aa36d47e6362a37e370541e49
Clyde Ian Acaso <clyde.acaso@cloudemployee.co.uk>
2024-10-28 17:07:23 +0800

    replaced reset icon

--- src/components/icons/DeleteIcon.tsx
+++ src/components/icons/DeleteIcon.tsx
@@ -19,14 +19,15 @@ export const DeleteIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   // </svg>
 
   <svg
-    stroke={props.color ?? defaultButtonColor}
-    fill="#000000"
-    width="24"
+    fill="#ffffff"
     height="24"
+    width="24"
     version="1.1"
     id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 512 512"
+    stroke="#ffffff"
+    stroke-width="7.68"
   >
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
